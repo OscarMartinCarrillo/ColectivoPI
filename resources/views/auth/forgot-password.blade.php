ce0a250f81bbf71128b87199bc9d405dec05dc6a
OscarMartinCarrillo <oscarmartinc24@gmail.com>
2021-06-06 17:42:34 +0200

    Procfile

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,10 +1,4 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ 'Colectivo Lancelot' }}
-        </h2>
-    </x-slot>
-
     <div class="container mt-3 mx-auto p-2 w-4/5">
         <x-auth-card>
             <x-slot name="logo">
